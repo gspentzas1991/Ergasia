05b7d44331d0452577427aad2bb0688b4ec971e0
Giannis Spentzas <gspentzas1991@gmail.com>
2018-01-12 18:30:46 +0200

    Changed comments to english

--- StringCheck.java
+++ StringCheck.java
@@ -1,13 +1,12 @@
 package seat_administration;
 
-/**I klasi tha periexei methodous pou
- * exoun na kanoun me Strings
+/**Contains the method that checks if a String has contains a number or characters
  */
 public final class StringCheck 
 {
 	
-	/** Pernei ena String ke elenxei an exei 
-	 * mesa keimeno i arithmous
+	/** Checks if the String str contains numbers or characters.
+	Returns true if it doesn't contain characters and false if it does
 	 */
 	public static boolean isNumeric(final String str)  
 	{  
